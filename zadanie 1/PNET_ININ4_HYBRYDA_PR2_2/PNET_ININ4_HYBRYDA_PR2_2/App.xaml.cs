79d523ce26f1a5fa7e723cbc02976d72dcbc6dec
Mikolaj Zblewski <77124583+zbleszczak@users.noreply.github.com>
2024-05-29 16:47:12 +0200

    zadanie 1

---
+++ zadanie 1/PNET_ININ4_HYBRYDA_PR2_2/PNET_ININ4_HYBRYDA_PR2_2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PNET_ININ4_HYBRYDA_PR2_2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
